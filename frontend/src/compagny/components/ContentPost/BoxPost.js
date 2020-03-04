dd7408486d8b334cff18330861a45fa54d2d4127
Jean Boungou <placide@localhost.localdomain>
2020-03-04 20:12:50 +0000

    deleted BoxPost card

--- frontend/src/compagny/components/ContentPost/BoxPost.js
+++ frontend/src/compagny/components/ContentPost/BoxPost.js
@@ -3,13 +3,7 @@ import React from 'react';
 const BoxPost =  () => {
         return (
             <div>
-                <div className="card">
-                    <div className="card-box shadow-sm p-3 mb-5 " Style={"color: #fff"}>
-                        <h1>Get Started</h1>
-                        <span>1 of 6</span>
-                    </div>
-                    <span>Enter the name of your job post</span>
-                </div>
+
             </div>
         );
 };
